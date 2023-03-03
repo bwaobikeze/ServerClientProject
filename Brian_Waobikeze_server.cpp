c0ad36c3038068dde964c3b322cb2dd92b94990b
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-03-02 22:30:13 -0600

    Initlizing Server & Client

---
+++ Brian_Waobikeze_server.cpp
@@ -0,0 +1,15 @@
+//
+// Created by brian waobikeze on 3/2/23.
+//
+
+#include <iostream>
+using namespace std;
+//
+int main() {
+    std::cout << "This is the server file" << std::endl;
+    return 0;
+}
+
+//int somefunct(){
+//    cout << "This is the server file" <<endl;
+//}
