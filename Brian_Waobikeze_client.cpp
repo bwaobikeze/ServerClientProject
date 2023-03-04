0a6b7632b587a299770dd4eb6a88f0289db01c07
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-03-03 22:36:58 -0600

    Past Attempt Rework

--- Brian_Waobikeze_client.cpp
+++ Brian_Waobikeze_client.cpp
@@ -1,40 +1,156 @@
-#include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <string>
+#include <iostream>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <netinet/in.h>
+#include <netdb.h>
+// Second COSC 3360 assignment for fall 2021
+//  Client part
+//
+// Brian Waobikeze
+// November 2021
+#define MSG_CONFIRM 0
 using namespace std;
 
-#define MAXLINE 1024
+int portno;
+struct hostent *server;
+// char clientMessage[];
+char buffer[256];
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
 
-int main() {
-    //std::cout << "Hello, World!" << std::endl;
-    int number;
-    char *localHost;
-    //string hp;
-    int PortNum;
-    int socketID;
-    struct sockaddr_in servaddr;
-
-    cin>>number;
-    cout<<"Please Type In Host Name: "<< endl;
-    cin>>localHost;
-    cout<<"Please enter A Port Number: "<<endl;
-    cin>>PortNum;
-    int hp=gethostname(localHost,MAXLINE);
-
-    if((socketID= socket(AF_INET,SOCK_DGRAM,0))<0){
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
+void promptHost()
+{
+    try
+    {
+        string hostName = "";
+        while (hostName != "localhost")
+        {
+            cout << "Enter the server hostname; typically 'localhost': ";
+            cin >> hostName;
+        }
+        cout << "Host name: " << hostName << endl; // check host name
+        int hostNameLength = hostName.length();
+        char hostNameCharArray[hostNameLength + 1];
+        strcpy(hostNameCharArray, hostName.c_str());
+        server = gethostbyname(hostNameCharArray);
     }
-    memset(&servaddr,0,sizeof (servaddr));
-    servaddr.sin_family=
-            servaddr.sin_port= htons(PortNum);
+    catch (...)
+    {
+        error("Error setting host name");
+    }
+}
 
-    //cout<<"Hello Number "<< number<<" Its Great to Have you!"<<endl;
+void promptPortNo()
+{
+    try
+    {
+        cout << "Enter the server portnumber: ";
+        cin >> portno;
+        cout << "Port number: " << portno << endl;
+    }
+    catch (...)
+    {
+        error("Error setting port number");
+    }
+}
 
+void promptLicensePlate()
+{
+    try
+    { // make sure total client message length is 18
+        string licenseNumber;
+        string localMessage;
+        while (1)
+        {
+            cout << "Enter License plate number: ";
+            cin >> licenseNumber;
+            cout << licenseNumber.size() << endl;
+            if (licenseNumber.size() != 9)
+//            {
+//                cout << "Invalid test id" << endl;
+//                continue;
+//            }
+            localMessage += licenseNumber + ":";
+            break;
+        }
+//        while (1)
+//        {
+//            cout << "Enter your birthday in MMDDYYYY format: ";
+//            cin >> birthday;
+//            if (birthday.size() != 8)
+//            {
+//                cout << "Invalid birthday" << endl;
+//                continue;
+//            }
+//            localMessage += birthday;
+//            break;
+//        }
+
+        cout << "Local message " << localMessage << endl;
+        int clientMessageLength = localMessage.length();
+        char clientMessageCharArray[clientMessageLength + 1];
+        strcpy(buffer, localMessage.c_str());
+    }
+    catch (...)
+    {
+        error("Error setting Licenses Plate");
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    socklen_t len;
+    int sockfd, n;
+    struct sockaddr_in serv_addr;
+    promptHost();
+    promptPortNo();
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    if (server == NULL)
+    {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+    memset(&serv_addr, 0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+    serv_addr.sin_port = htons(portno);
+//    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+//        error("ERROR connecting");
+//    bzero(buffer, 256);
+    promptLicensePlate();
+    sendto(sockfd, (const char *)buffer, strlen(buffer),
+           MSG_CONFIRM, (const struct sockaddr *) &serv_addr,
+           sizeof(serv_addr));
+    // fgets(buffer, 255, stdin); // get input from stdin
+    //n = write(sockfd, buffer, strlen(buffer));
+//    if (n < 0)
+//        error("ERROR writing to socket");
+//    bzero(buffer, 256);
+    //n = read(sockfd, buffer, 255);
+    n=recvfrom(sockfd, (char *)buffer, 255,
+               MSG_WAITALL, (struct sockaddr *) &serv_addr,
+               &len);
+    if (n < 0)
+        error("ERROR reading from socket");
+    buffer[n] = '\0';
+//    if (buffer[0] == 'N')
+//        cout << "Your test result was NEGATIVE." << endl;
+//    else if (buffer[0] == 'P')
+//        cout << "Your test result was POSITIVE" << endl;
+//    else
+//        cout << "The test result you requested is not in our database." << endl;
+    close(sockfd);
     return 0;
 }
--- Brian_Waobikeze_server.cpp
+++ Brian_Waobikeze_server.cpp
@@ -1,15 +1,208 @@
-//
-// Created by brian waobikeze on 3/2/23.
-//
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
 #include <iostream>
-using namespace std;
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+// Second COSC 3360 assignment for fall 2021
+// Server part
 //
-int main() {
-    std::cout << "This is the server file" << std::endl;
-    return 0;
+// Brian Waobikeze
+// November 2021
+
+
+using namespace std;
+
+struct testResult
+{
+    string testID;
+    string birthday;
+    string result;
+};
+
+vector<testResult> allTestResults;
+int portno;
+char clientResult[1];
+int clientMessageLength = 18;
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
 }
 
-//int somefunct(){
-//    cout << "This is the server file" <<endl;
-//}
+void printAllTestResults()
+{
+    if (allTestResults.size() < 1)
+        error("All results vector is empty");
+    for (int i = 0; i < allTestResults.size(); i++)
+    {
+        cout << "testID: " + allTestResults[i].testID << endl;
+        cout << "birthday: " + allTestResults[i].birthday << endl;
+        cout << "result: " + allTestResults[i].result << endl;
+        cout << "==========\n";
+    }
+}
+
+void formatResults(string line)
+{
+    vector<string> currentLine;
+    testResult currentTestResult;
+    istringstream iss(line);
+    string firstSplitter;
+    string secondSplitter;
+    getline(iss, firstSplitter, '\n');
+    istringstream sendStream(firstSplitter);
+    while (getline(sendStream, secondSplitter, ' '))
+    {
+        if (!secondSplitter.empty())
+        {
+            currentLine.push_back(secondSplitter);
+        }
+        // clears vector if full
+        if (currentLine.size() > 2)
+        {
+            // fill struct content
+            testResult newTestResult;
+            newTestResult.testID = currentLine[0];
+            newTestResult.birthday = currentLine[1];
+            newTestResult.result = currentLine[2];
+            allTestResults.push_back(newTestResult);
+            currentLine.clear();
+        }
+    }
+}
+
+void readResults(string inputPath)
+{
+    fstream fileVar;
+    fileVar.open(inputPath);
+    if (!fileVar.is_open())
+    {
+        error("Unable to read results");
+    }
+    string line;
+    while (getline(fileVar, line))
+    {
+        formatResults(line);
+    }
+    printAllTestResults();
+}
+
+void getTestResult(string testID, string birthday)
+{
+    cout << "getting results for: " << testID << ":" << birthday << endl;
+    if (testID.size() < 1 || birthday.size() < 1)
+        return;
+    for (int i = 0; i < allTestResults.size(); i++)
+    {
+
+        if (allTestResults[i].testID.compare(testID) == 0)
+        {
+            cout << "Test ID: " << allTestResults[i].testID << endl;
+            cout << "Birthday: " << allTestResults[i].birthday << endl;
+            cout << "Test result: " << allTestResults[i].result << endl;
+            clientResult[0] = allTestResults[i].result[0];
+        }
+    }
+}
+
+void promptPortNumber()
+{
+    try
+    {
+        cout << "Enter the server port number: ";
+        cin >> portno;
+    }
+    catch (...)
+    {
+        error("Error setting port number");
+    }
+}
+
+void getClientResponse(char clientMessage[])
+{
+    try
+    {
+        string testID;
+        string birthday;
+        int splitterIDX = -1;
+        for (int i = 0; i < clientMessageLength;
+             i++)
+        {
+            if (clientMessage[i] != ' ')
+            {
+                if (clientMessage[i] == ':')
+                {
+                    splitterIDX = i;
+                    continue;
+                }
+                if (splitterIDX != -1 && i > splitterIDX)
+                {
+                    birthday += clientMessage[i];
+                }
+                else
+                {
+                    testID += clientMessage[i];
+                }
+            }
+        }
+        getTestResult(testID, birthday);
+    }
+    catch (...)
+    {
+        cout << "Error attempting to get your result" << endl;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    readResults("./testResults20.txt");
+
+    int sockfd, newsockfd, n;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); // creates an internet socket of stream type
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    bzero((char *)&serv_addr, sizeof(serv_addr)); // sets all values in buffer  to 0
+    // portno = atoi(argv[1]);                       // sets port number for server to listen
+    promptPortNumber();
+    // configure server  addres struct
+    serv_addr.sin_family = AF_INET;         // address family
+    serv_addr.sin_addr.s_addr = INADDR_ANY; // host IP address
+    serv_addr.sin_port = htons(portno);     // host port number
+    if (bind(sockfd, (struct sockaddr *)&serv_addr,
+             sizeof(serv_addr)) < 0) // bind address of host and port
+        error("ERROR on binding");
+    listen(sockfd, 5); // listen on socket for connection
+    while (1)
+    {
+        clilen = sizeof(cli_addr);
+        newsockfd = accept(sockfd,
+                           (struct sockaddr *)&cli_addr,
+                           &clilen); // wakes up process when client connects
+        if (newsockfd < 0)
+            error("ERROR on accept");
+        // runs when client connects to server
+        bzero(buffer, 256);
+        n = read(newsockfd, buffer, 255);
+        if (n < 0)
+            error("ERROR reading from socket");
+        printf("Here is the message: %s\n", buffer);
+        getClientResponse(buffer);
+        n = write(newsockfd, clientResult, 18);
+        if (n < 0)
+            error("ERROR writing to socket");
+        close(newsockfd);
+    }
+    close(sockfd);
+    return 0;
+}
