5124aab9b61c77104f7177f4e2a0242466486786
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-03-14 23:40:00 -0500

    worrking on server

--- Brian_Waobikeze_server.cpp
+++ Brian_Waobikeze_server.cpp
@@ -19,14 +19,7 @@
 
 using namespace std;
 
-struct testResult
-{
-    string testID;
-    string birthday;
-    string result;
-};
-
-vector<testResult> allTestResults;
+vector<string> allTestResults;
 int portno;
 char clientResult[1];
 int clientMessageLength = 18;
@@ -42,9 +35,7 @@ void printAllTestResults()
         error("All results vector is empty");
     for (int i = 0; i < allTestResults.size(); i++)
     {
-        cout << "testID: " + allTestResults[i].testID << endl;
-        cout << "birthday: " + allTestResults[i].birthday << endl;
-        cout << "result: " + allTestResults[i].result << endl;
+        cout << "License # "<< i<< ": "<< allTestResults[i] << endl;
         cout << "==========\n";
     }
 }
@@ -52,30 +43,14 @@ void printAllTestResults()
 void formatResults(string line)
 {
     vector<string> currentLine;
-    testResult currentTestResult;
-    istringstream iss(line);
-    string firstSplitter;
-    string secondSplitter;
-    getline(iss, firstSplitter, '\n');
-    istringstream sendStream(firstSplitter);
-    while (getline(sendStream, secondSplitter, ' '))
-    {
-        if (!secondSplitter.empty())
-        {
-            currentLine.push_back(secondSplitter);
-        }
-        // clears vector if full
-        if (currentLine.size() > 2)
-        {
-            // fill struct content
-            testResult newTestResult;
-            newTestResult.testID = currentLine[0];
-            newTestResult.birthday = currentLine[1];
-            newTestResult.result = currentLine[2];
-            allTestResults.push_back(newTestResult);
-            currentLine.clear();
-        }
-    }
+//    testResult currentTestResult;
+//    istringstream iss(line);
+//    string firstSplitter;
+//    string secondSplitter;
+//    getline(iss, firstSplitter, '\n');
+//    istringstream sendStream(firstSplitter);
+    //currentLine.push_back(line);
+    allTestResults.push_back(line);
 }
 
 void readResults(string inputPath)
@@ -91,26 +66,26 @@ void readResults(string inputPath)
     {
         formatResults(line);
     }
-    printAllTestResults();
+    //printAllTestResults();
 }
 
-void getTestResult(string testID, string birthday)
-{
-    cout << "getting results for: " << testID << ":" << birthday << endl;
-    if (testID.size() < 1 || birthday.size() < 1)
-        return;
-    for (int i = 0; i < allTestResults.size(); i++)
-    {
-
-        if (allTestResults[i].testID.compare(testID) == 0)
-        {
-            cout << "Test ID: " << allTestResults[i].testID << endl;
-            cout << "Birthday: " << allTestResults[i].birthday << endl;
-            cout << "Test result: " << allTestResults[i].result << endl;
-            clientResult[0] = allTestResults[i].result[0];
-        }
-    }
-}
+//void getTestResult(string testID, string birthday)
+//{
+//    cout << "getting results for: " << testID << ":" << birthday << endl;
+//    if (testID.size() < 1 || birthday.size() < 1)
+//        return;
+//    for (int i = 0; i < allTestResults.size(); i++)
+//    {
+//
+//        if (allTestResults[i].testID.compare(testID) == 0)
+//        {
+//            cout << "Test ID: " << allTestResults[i].testID << endl;
+//            cout << "Birthday: " << allTestResults[i].birthday << endl;
+//            cout << "Test result: " << allTestResults[i].result << endl;
+//            clientResult[0] = allTestResults[i].result[0];
+//        }
+//    }
+//}
 
 void promptPortNumber()
 {
@@ -125,51 +100,51 @@ void promptPortNumber()
     }
 }
 
-void getClientResponse(char clientMessage[])
-{
-    try
-    {
-        string testID;
-        string birthday;
-        int splitterIDX = -1;
-        for (int i = 0; i < clientMessageLength;
-             i++)
-        {
-            if (clientMessage[i] != ' ')
-            {
-                if (clientMessage[i] == ':')
-                {
-                    splitterIDX = i;
-                    continue;
-                }
-                if (splitterIDX != -1 && i > splitterIDX)
-                {
-                    birthday += clientMessage[i];
-                }
-                else
-                {
-                    testID += clientMessage[i];
-                }
-            }
-        }
-        getTestResult(testID, birthday);
-    }
-    catch (...)
-    {
-        cout << "Error attempting to get your result" << endl;
-    }
-}
+//void getClientResponse(char clientMessage[])
+//{
+//    try
+//    {
+//        string testID;
+//        string birthday;
+//        int splitterIDX = -1;
+//        for (int i = 0; i < clientMessageLength;
+//             i++)
+//        {
+//            if (clientMessage[i] != ' ')
+//            {
+//                if (clientMessage[i] == ':')
+//                {
+//                    splitterIDX = i;
+//                    continue;
+//                }
+//                if (splitterIDX != -1 && i > splitterIDX)
+//                {
+//                    birthday += clientMessage[i];
+//                }
+//                else
+//                {
+//                    testID += clientMessage[i];
+//                }
+//            }
+//        }
+//        getTestResult(testID, birthday);
+//    }
+//    catch (...)
+//    {
+//        cout << "Error attempting to get your result" << endl;
+//    }
+//}
 
 int main(int argc, char *argv[])
 {
-    readResults("./testResults20.txt");
+    readResults("/Users/brianwaobikeze/Desktop/ServerClientProj2023/read.txt");
 
     int sockfd, newsockfd, n;
     socklen_t clilen;
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); // creates an internet socket of stream type
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0); // creates an internet socket of stream type
     if (sockfd < 0)
         error("ERROR opening socket");
     bzero((char *)&serv_addr, sizeof(serv_addr)); // sets all values in buffer  to 0
@@ -179,30 +154,31 @@ int main(int argc, char *argv[])
     serv_addr.sin_family = AF_INET;         // address family
     serv_addr.sin_addr.s_addr = INADDR_ANY; // host IP address
     serv_addr.sin_port = htons(portno);     // host port number
-    if (bind(sockfd, (struct sockaddr *)&serv_addr,
-             sizeof(serv_addr)) < 0) // bind address of host and port
+    if ( ::bind(sockfd, (const struct sockaddr *)&serv_addr,
+              sizeof(serv_addr)) < 0 ) {
+        // bind address of host and port
         error("ERROR on binding");
-    listen(sockfd, 5); // listen on socket for connection
-    while (1)
-    {
-        clilen = sizeof(cli_addr);
-        newsockfd = accept(sockfd,
-                           (struct sockaddr *)&cli_addr,
-                           &clilen); // wakes up process when client connects
-        if (newsockfd < 0)
-            error("ERROR on accept");
-        // runs when client connects to server
-        bzero(buffer, 256);
-        n = read(newsockfd, buffer, 255);
-        if (n < 0)
-            error("ERROR reading from socket");
-        printf("Here is the message: %s\n", buffer);
-        getClientResponse(buffer);
-        n = write(newsockfd, clientResult, 18);
-        if (n < 0)
-            error("ERROR writing to socket");
-        close(newsockfd);
     }
-    close(sockfd);
+//    while (1)
+//    {
+//        clilen = sizeof(cli_addr);
+//        newsockfd = accept(sockfd,
+//                           (struct sockaddr *)&cli_addr,
+//                           &clilen); // wakes up process when client connects
+//        if (newsockfd < 0)
+//            error("ERROR on accept");
+//        // runs when client connects to server
+//        bzero(buffer, 256);
+//        n = read(newsockfd, buffer, 255);
+//        if (n < 0)
+//            error("ERROR reading from socket");
+//        printf("Here is the message: %s\n", buffer);
+//        //getClientResponse(buffer);
+//        n = write(newsockfd, clientResult, 18);
+//        if (n < 0)
+//            error("ERROR writing to socket");
+//        close(newsockfd);
+//    }
+   close(sockfd);
     return 0;
 }
